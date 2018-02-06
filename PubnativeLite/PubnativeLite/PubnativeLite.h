ce25ef7618bd2701616e9447240bc58aac46d640
Can Soykarafakılı <can.soykarafakili@gmail.com>
2018-02-06 12:52:09 +0100

    Project Creation

---
+++ PubnativeLite/PubnativeLite/PubnativeLite.h
@@ -0,0 +1,19 @@
+//
+//  PubnativeLite.h
+//  PubnativeLite
+//
+//  Created by Can Soykarafakili on 06.02.18.
+//  Copyright © 2018 Can Soykarafakili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PubnativeLite.
+FOUNDATION_EXPORT double PubnativeLiteVersionNumber;
+
+//! Project version string for PubnativeLite.
+FOUNDATION_EXPORT const unsigned char PubnativeLiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PubnativeLite/PublicHeader.h>
+
+
