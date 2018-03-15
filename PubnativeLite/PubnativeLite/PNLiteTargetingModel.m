c4d6dd6a890e077e39f0081c95dcf010746c4228
Can Soykarafakılı <can.soykarafakili@gmail.com>
2018-03-15 17:26:46 +0100

    Removed unused params & methods in PNLiteTargetingModel

--- PubnativeLite/PubnativeLite/PNLiteTargetingModel.m
+++ PubnativeLite/PubnativeLite/PNLiteTargetingModel.m
@@ -22,41 +22,13 @@
 
 #import "PNLiteTargetingModel.h"
 
-NSString *const kPNLiteTargetingModelGenderFemale = @"f";
-NSString *const kPNLiteTargetingModelGenderMale = @"m";
-
 @implementation PNLiteTargetingModel
 
 - (void)dealloc
 {
     self.age = nil;
-    self.education = nil;
     self.interests = nil;
     self.gender = nil;
-    self.iap = nil;
-    self.iap_total = nil;
-}
-
-- (NSDictionary *)toDictionary
-{
-    NSMutableDictionary *result = [NSMutableDictionary dictionary];
-    
-    result[@"age"] = [self.age stringValue];
-    result[@"education"] = self.education;
-    result[@"interests"] = [self.interests componentsJoinedByString:@","];
-    result[@"gender"] = self.gender;
-    
-    return result;
-}
-
-- (NSDictionary *)toDictionaryWithIAP
-{
-    NSMutableDictionary *result = [[self toDictionary] mutableCopy];
-    
-    result[@"iap"] = [self.iap stringValue];
-    result[@"iap_total"] = [self.iap_total stringValue];
-    
-    return result;
 }
 
 @end
