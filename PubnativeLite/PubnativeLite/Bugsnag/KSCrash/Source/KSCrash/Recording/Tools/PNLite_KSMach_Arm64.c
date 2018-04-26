8700f50d536943d991adfd0be3a9da1e328adea6
Can Soykarafakılı <can.soykarafakili@gmail.com>
2018-04-26 17:54:04 +0200

    BSG_KSMach_Arm64 changed

--- PubnativeLite/PubnativeLite/Bugsnag/KSCrash/Source/KSCrash/Recording/Tools/PNLite_KSMach_Arm64.c
+++ PubnativeLite/PubnativeLite/Bugsnag/KSCrash/Source/KSCrash/Recording/Tools/PNLite_KSMach_Arm64.c
@@ -1,27 +1,23 @@
 //
-//  KSMach_Arm.c
+//  Copyright © 2018 PubNative. All rights reserved.
 //
-//  Created by Karl Stenerud on 2013-09-29.
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
 //
-//  Copyright (c) 2012 Karl Stenerud. All rights reserved.
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
 //
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall remain in place
-// in this source code.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 #if defined(__arm64__)
@@ -31,18 +27,18 @@
 //#define PNLite_KSLogger_LocalLevel TRACE
 #include "PNLite_KSLogger.h"
 
-static const char *bsg_g_registerNames[] = {
+static const char *pnlite_g_registerNames[] = {
     "x0",  "x1",  "x2",  "x3",  "x4",  "x5",  "x6",  "x7",  "x8",
     "x9",  "x10", "x11", "x12", "x13", "x14", "x15", "x16", "x17",
     "x18", "x19", "x20", "x21", "x22", "x23", "x24", "x25", "x26",
     "x27", "x28", "x29", "fp",  "lr",  "sp",  "pc",  "cpsr"};
-static const int bsg_g_registerNamesCount =
-    sizeof(bsg_g_registerNames) / sizeof(*bsg_g_registerNames);
+static const int pnlite_g_registerNamesCount =
+    sizeof(pnlite_g_registerNames) / sizeof(*pnlite_g_registerNames);
 
-static const char *bsg_g_exceptionRegisterNames[] = {"exception", "esr", "far"};
-static const int bsg_g_exceptionRegisterNamesCount =
-    sizeof(bsg_g_exceptionRegisterNames) /
-    sizeof(*bsg_g_exceptionRegisterNames);
+static const char *pnlite_g_exceptionRegisterNames[] = {"exception", "esr", "far"};
+static const int pnlite_g_exceptionRegisterNamesCount =
+    sizeof(pnlite_g_exceptionRegisterNames) /
+    sizeof(*pnlite_g_exceptionRegisterNames);
 
 uintptr_t
 bsg_ksmachframePointer(const PNLite_STRUCT_MCONTEXT_L *const machineContext) {
@@ -83,11 +79,11 @@ bool bsg_ksmachexceptionState(const thread_t thread,
                                ARM_EXCEPTION_STATE64_COUNT);
 }
 
-int bsg_ksmachnumRegisters(void) { return bsg_g_registerNamesCount; }
+int bsg_ksmachnumRegisters(void) { return pnlite_g_registerNamesCount; }
 
 const char *bsg_ksmachregisterName(const int regNumber) {
     if (regNumber < bsg_ksmachnumRegisters()) {
-        return bsg_g_registerNames[regNumber];
+        return pnlite_g_registerNames[regNumber];
     }
     return NULL;
 }
@@ -117,12 +113,12 @@ bsg_ksmachregisterValue(const PNLite_STRUCT_MCONTEXT_L *const machineContext,
 }
 
 int bsg_ksmachnumExceptionRegisters(void) {
-    return bsg_g_exceptionRegisterNamesCount;
+    return pnlite_g_exceptionRegisterNamesCount;
 }
 
 const char *bsg_ksmachexceptionRegisterName(const int regNumber) {
     if (regNumber < bsg_ksmachnumExceptionRegisters()) {
-        return bsg_g_exceptionRegisterNames[regNumber];
+        return pnlite_g_exceptionRegisterNames[regNumber];
     }
     PNLite_KSLOG_ERROR("Invalid register number: %d", regNumber);
     return NULL;
