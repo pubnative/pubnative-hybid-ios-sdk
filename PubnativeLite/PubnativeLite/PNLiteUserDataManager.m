68bdb38030001f13ddab5e9179801cdb152dfa29
Can Soykarafakılı <can.soykarafakili@gmail.com>
2018-05-18 18:35:40 +0200

    Working on PNLiteUserDataManager

---
+++ PubnativeLite/PubnativeLite/PNLiteUserDataManager.m
@@ -0,0 +1,150 @@
+//
+//  Copyright © 2018 PubNative. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+#import "PNLiteUserDataManager.h"
+#import "PNLiteSettings.h"
+#import "PNLiteGeoIPRequest.h"
+#import "PNLiteCountryUtils.h"
+
+NSString *const kPNLiteGDPRConsentUUIDKey = @"gdpr_consent_uuid";
+NSString *const kPNLiteGDPRConsentStateKey = @"gdpr_consent_state";
+NSString *const kPNLitePrivacyPolicyUrl = @"https://pubnative.net/privacy-policy/";
+NSString *const kPNLiteVendorListUrl = @"https://pubnative.net/vendor-list/";
+NSInteger const kPNLiteConsentStateAccepted = 1;
+NSInteger const kPNLiteConsentStateDenied = 0;
+
+@interface PNLiteUserDataManager () <PNLiteGeoIPRequestDelegate>
+
+@property (nonatomic, assign) BOOL inGDPRZone;
+@property (nonatomic, assign) BOOL initialisedSuccessfully;
+@property (nonatomic, assign) NSInteger consentState;
+@property (nonatomic, strong) NSString *UUID;
+@property (nonatomic, weak) NSObject <PNLiteUserDataManagerDelegate> *delegate;
+
+@end
+
+@implementation PNLiteUserDataManager
+
+- (void)dealloc
+{
+    self.UUID = nil;
+}
+
+- (instancetype)initWithAppToken:(NSString *)appToken withDelegate:(NSObject<PNLiteUserDataManagerDelegate> *)delegate
+{
+    self = [super init];
+    if (self) {
+        self.inGDPRZone = NO;
+        self.initialisedSuccessfully = NO;
+        self.consentState = kPNLiteConsentStateDenied;
+        self.delegate = delegate;
+    }
+    return self;
+}
+
+- (void)determineUserZoneWithDelegate:(NSObject<PNLiteUserDataManagerDelegate> *)delegate
+{
+    PNLiteGeoIPRequest *request = [[PNLiteGeoIPRequest alloc] init];
+    [request requestGeoIPWithDelegate:self];
+}
+
+- (NSString *)privacyPolicyLink
+{
+    return kPNLitePrivacyPolicyUrl;
+}
+
+- (NSString *)vendorListLink
+{
+    return kPNLiteVendorListUrl;
+}
+
+- (BOOL)shouldAskConsent
+{
+    return [self GDPRApplies] && ![self GDPRConsentAsked];
+}
+
+- (void)grantConsent
+{
+    self.consentState = kPNLiteConsentStateAccepted;
+    [self saveGDPRConsentState];
+    
+    //TODO sync with API
+}
+
+- (void)denyConsent
+{
+    self.consentState = kPNLiteConsentStateDenied;
+    [self saveGDPRConsentState];
+    
+    //TODO sync with API
+}
+
+- (void)revokeConsent
+{
+    self.consentState = kPNLiteConsentStateDenied;
+    [self saveGDPRConsentState];
+    
+    //TODO sync with API
+}
+
+- (void)saveGDPRConsentState
+{
+    [[NSUserDefaults standardUserDefaults] setInteger:self.consentState forKey:kPNLiteGDPRConsentStateKey];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+- (void)saveGDPRConsentUUID
+{
+    [[NSUserDefaults standardUserDefaults] setObject:self.UUID forKey:kPNLiteGDPRConsentUUIDKey];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
+- (BOOL)GDPRApplies
+{
+    return self.inGDPRZone;
+}
+
+- (BOOL)GDPRConsentAsked
+{
+    return [[NSUserDefaults standardUserDefaults] objectForKey:kPNLiteGDPRConsentStateKey];
+}
+
+#pragma mark PNLiteGeoIPRequestDelegate
+
+- (void)requestDidStart:(PNLiteGeoIPRequest *)request
+{
+    
+}
+
+- (void)request:(PNLiteGeoIPRequest *)request didLoadWithGeoIP:(PNLiteGeoIPModel *)geoIP
+{
+    self.inGDPRZone = [PNLiteCountryUtils isGDPRCountry:geoIP.countryCode];
+    self.initialisedSuccessfully = YES;
+    [self.delegate userDataInitialised:self.initialisedSuccessfully];
+}
+
+- (void)request:(PNLiteGeoIPRequest *)request didFailWithError:(NSError *)error
+{
+    
+}
+
+@end
