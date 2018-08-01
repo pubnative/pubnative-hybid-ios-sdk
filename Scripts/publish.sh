f7d8d13a2c278267ced9822ef9a5683f04fbd96a
Can Soykarafakılı <can.soykarafakili@gmail.com>
2018-08-01 14:50:53 +0200

    gymfile removed

--- Scripts/publish.sh
+++ Scripts/publish.sh
@@ -15,5 +15,6 @@ agvtool -noscm new-marketing-version "$(agvtool what-marketing-version -terse1)-
 agvtool new-version -all $CIRCLE_BUILD_NUM
 cd ..
 bundle exec fastlane adhoc --verbose
+bundle exec fastlane gym --verbose --include_bitcode true --include_symbols true --clean --workspace PubnativeLite.xcworkspace --scheme PubnativeLiteDemo --archive_path $ARCHIVE_PATH --output_directory $OUTPUT_FOLDER --export_options $OPTIONS_PLIST
 # Upload generated IPA to Fabric
 ./scripts/submit $FABRIC_API_KEY $FABRIC_API_SECRET -ipaPath $OUTPUT_FOLDER/PubnativeLiteDemo.ipa
