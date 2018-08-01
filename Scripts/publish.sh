43c9224f41d1e9598a517171525f7da914a79f4b
Can Soykarafakılı <can.soykarafakili@gmail.com>
2018-08-01 14:21:06 +0200

    gymfile created

--- Scripts/publish.sh
+++ Scripts/publish.sh
@@ -6,15 +6,12 @@ OUTPUT_FOLDER=$BASE_FOLDER/ipa
 rm $OPTIONS_PLIST
 rm -rf $ARCHIVE_PATH
 rm -rf $OUTPUT_FOLDER
-#GENERATE PLIST
-PLIST='{"compileBitcode":false,"method":"ad-hoc","ProvisioningStyle": "Manual"}'
-echo $PLIST | plutil -convert xml1 -o $OPTIONS_PLIST -
 #GENERATE ARCHIVE
 cd PubnativeLite
 agvtool -noscm new-marketing-version "$(agvtool what-marketing-version -terse1)-${CIRCLE_BRANCH}.${CIRCLE_BUILD_NUM}"
 agvtool new-version -all $CIRCLE_BUILD_NUM
 cd ..
 bundle exec fastlane adhoc --verbose
-bundle exec fastlane gym --verbose --include_bitcode true --include_symbols true --clean --workspace PubnativeLite.xcworkspace --scheme PubnativeLiteDemo --archive_path $ARCHIVE_PATH --output_directory $OUTPUT_FOLDER --export_options $OPTIONS_PLIST
+bundle exec fastlane gym --verbose
 # Upload generated IPA to Fabric
 ./scripts/submit $FABRIC_API_KEY $FABRIC_API_SECRET -ipaPath $OUTPUT_FOLDER/PubnativeLiteDemo.ipa
