a1cef5d10924478feb58ce1fb83b833a1c8c956c
Can Soykarafakılı <can.soykarafakili@gmail.com>
2018-10-08 12:18:48 +0200

    unnecessary space removed

--- PubnativeLite/PubnativeLiteDemo/AppDelegate.m
+++ PubnativeLite/PubnativeLiteDemo/AppDelegate.m
@@ -32,7 +32,6 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     [HyBid initWithAppToken:[PNLiteDemoSettings sharedInstance].appToken completion:^(BOOL success) {
