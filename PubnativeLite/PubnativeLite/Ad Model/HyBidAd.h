2674fd45ea7dec790bcc2f4d87e34b92915116e9
Can Soykarafakılı <can.soykarafakili@gmail.com>
2020-03-05 16:14:33 +0100

    Getting width & height value from AdResponse for sizing MRAIDView
    Creating a readonly properties for both.

--- PubnativeLite/PubnativeLite/Ad Model/HyBidAd.h
+++ PubnativeLite/PubnativeLite/Ad Model/HyBidAd.h
@@ -35,6 +35,9 @@
 @property (nonatomic, readonly) NSString *creativeID;
 @property (nonatomic, readonly) NSNumber *assetGroupID;
 @property (nonatomic, readonly) NSNumber *eCPM;
+@property (nonatomic, readonly) NSNumber *width;
+@property (nonatomic, readonly) NSNumber *height;
+
 @property (nonatomic, readonly) NSArray<HyBidDataModel*> *beacons;
 @property (nonatomic, readonly) HyBidContentInfoView *contentInfo;
 
--- PubnativeLite/PubnativeLite/Ad Model/HyBidDataModel.h
+++ PubnativeLite/PubnativeLite/Ad Model/HyBidDataModel.h
@@ -33,6 +33,8 @@
 @property (nonatomic, readonly) NSString *url;
 @property (nonatomic, readonly) NSString *html;
 @property (nonatomic, readonly) NSNumber *eCPM;
+@property (nonatomic, readonly) NSNumber *width;
+@property (nonatomic, readonly) NSNumber *height;
 
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary;
 - (NSString *)stringFieldWithKey:(NSString *)key;
