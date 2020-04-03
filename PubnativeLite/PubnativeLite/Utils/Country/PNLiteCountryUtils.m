62d32df32e1ef21409149f9c480571537957d9a0
Can Soykarafakılı <can.soykarafakili@gmail.com>
2020-04-03 12:39:04 +0200

    Add new country codes in GDPR countries. Bump SDK version to 1.4.5

--- PubnativeLite/PubnativeLite/Utils/Country/PNLiteCountryUtils.m
+++ PubnativeLite/PubnativeLite/Utils/Country/PNLiteCountryUtils.m
@@ -30,7 +30,7 @@
 }
 
 + (NSArray *)gdprCountries {
-    return [NSArray arrayWithObjects:@"BE", @"EL", @"LT", @"PT", @"BG", @"ES", @"LU", @"RO", @"CZ", @"FR", @"HU", @"SI", @"DK", @"HR", @"MT", @"SK", @"DE", @"IT", @"NL", @"FI", @"EE", @"CY", @"AT", @"SE", @"IE", @"LV", @"PL", @"UK", @"CH", @"NO", @"IS", @"LI", nil];
+    return [NSArray arrayWithObjects:@"BE", @"EL", @"LT", @"PT", @"BG", @"ES", @"LU", @"RO", @"CZ", @"FR", @"HU", @"SI", @"DK", @"HR", @"MT", @"SK", @"DE", @"IT", @"NL", @"FI", @"EE", @"CY", @"AT", @"SE", @"IE", @"LV", @"PL", @"UK", @"GB", @"CH", @"NO", @"IS", @"LI", nil];
 }
 
 @end
