7ebc7dfd7b68eb5de27da9018b71667e221f5680
Fares Benhamouda <fares.bnhamouda@gmail.com>
2020-04-09 17:30:59 +0200

    Renaming targets + fixing objective c target.

---
+++ HyBidDemo/HyBidDemo/Controllers/MRectViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MRectViewController.swift
+//  HyBidDemo
+//
+//  Created by Fares Ben Hamouda on 09.04.20.
+//  Copyright © 2020 Fares Ben Hamouda. All rights reserved.
+//
+
+import UIKit
+
+class MRectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- HyBidDemo/HyBidDemoObjc/Controllers/MRectViewController.m
+++ HyBidDemo/HyBidDemoObjc/Controllers/MRectViewController.m
@@ -1,13 +1,82 @@
 //
-//  MRectViewController.m
-//  HyBidDemoObjc
+//  Copyright © 2018 PubNative. All rights reserved.
 //
-//  Created by Fares Ben Hamouda on 09.04.20.
-//  Copyright © 2020 Fares Ben Hamouda. All rights reserved.
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 #import "MRectViewController.h"
+#import <HyBid/HyBid.h>
+
+@interface MRectViewController () <HyBidAdViewDelegate>
+
+@property (weak, nonatomic) IBOutlet HyBidAdView *bannerAdView;
+@property (weak, nonatomic) IBOutlet UIButton *loadAdButton;
+@end
 
 @implementation MRectViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (IBAction)requestBannerTouchUpInside:(id)sender {
+    [self requestAd];
+}
+
+- (void)requestAd {
+    self.bannerAdView.adSize = HyBidAdSize.SIZE_300x250;
+    [self.bannerAdView loadWithDelegate:self];
+}
+
+- (void)showAlertControllerWithMessage:(NSString *)message {
+    UIAlertController *alertController = [UIAlertController
+                                          alertControllerWithTitle:@"I have a bad feeling about this... 🙄"
+                                          message:message
+                                          preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction * dismissAction = [UIAlertAction actionWithTitle:@"Dismiss" style:UIAlertActionStyleCancel handler:nil];
+    UIAlertAction *retryAction = [UIAlertAction actionWithTitle:@"Retry" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [self requestAd];
+    }];
+    [alertController addAction:dismissAction];
+    [alertController addAction:retryAction];
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
+#pragma mark - HyBidAdViewDelegate
+
+- (void)adViewDidLoad:(HyBidAdView *)adView {
+    NSLog(@"Banner Ad View did load:");
+    [adView show];
+}
+
+- (void)adView:(HyBidAdView *)adView didFailWithError:(NSError *)error {
+    NSLog(@"Banner Ad View did fail with error: %@",error.localizedDescription);
+    [self showAlertControllerWithMessage:error.localizedDescription];
+}
+
+- (void)adViewDidTrackClick:(HyBidAdView *)adView {
+    NSLog(@"Banner Ad View did track click:");
+}
+
+- (void)adViewDidTrackImpression:(HyBidAdView *)adView {
+    NSLog(@"Banner Ad View did track impression:");
+}
 
 @end
