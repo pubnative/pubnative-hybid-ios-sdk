cf5c775cf69d520c56009bc7c68cec91344277f3
Can Soykarafakılı <can.soykarafakili@gmail.com>
2020-04-23 17:05:04 +0200

    necessary headers are added to HyBidStatic.h

--- PubnativeLite/PubnativeLite/HyBidStatic.h
+++ PubnativeLite/PubnativeLite/HyBidStatic.h
@@ -75,6 +75,8 @@ FOUNDATION_EXPORT const unsigned char HyBidVersionString[];
 #import <HyBidStatic/VWAdSize.h>
 #import <HyBidStatic/VWInterstitialAd.h>
 #import <HyBidStatic/VWAdLibrary.h>
+#import <HyBid/VWInterstitialVideoAd.h>
+#import <HyBid/VWVideoAdRequest.h>
 
 typedef void (^HyBidCompletionBlock)(BOOL);
 
