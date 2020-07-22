404f6f52e40520e170c52af65955691f02e12ca4
Fares Ben Hamouda <fares.bnhamouda@gmail.com>
2020-07-22 16:47:55 +0200

    cleanup: remove testing data + remove check for rawResponse.
    (cherry picked from commit b4288485eb096a3ccc4be42778f01b69c2ce8214)

--- PubnativeLite/PubnativeLite/Ad Model/HyBidAd.m
+++ PubnativeLite/PubnativeLite/Ad Model/HyBidAd.m
@@ -83,16 +83,6 @@ NSString *const kImpressionQuerryParameter = @"t";
     if (data) {
         result = data.html;
     }
-    
-//    NSString *path = [[NSBundle mainBundle] pathForResource:@"ad8" ofType:@"txt"];
-//    NSError *error;
-//    NSString *content = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:&error];
-//
-//    if (error)
-//        NSLog(@"Error reading file: %@", error.localizedDescription);
-//
-//    NSLog(@"contents: %@", content);
-//    return content
     return result;
 }
 
