0036ae682aeac7061cb0a640cc059b12f8ad6147
eros902002@googlemail.com <eros902002@googlemail.com>
2020-09-28 16:45:39 +0200

    Add top view controller extension to native ad

--- PubnativeLite/PubnativeLite/Ad Model/HyBidNativeAd.m
+++ PubnativeLite/PubnativeLite/Ad Model/HyBidNativeAd.m
@@ -27,6 +27,7 @@
 #import "PNLiteImpressionTracker.h"
 #import "HyBidLogger.h"
 #import "HyBidSkAdNetworkModel.h"
+#import "UIApplication+PNLiteTopViewController.h"
 #import <WebKit/WebKit.h>
 #import "HyBidSKAdNetworkViewController.h"
 
@@ -266,10 +267,7 @@ NSString * const PNLiteNativeAdBeaconClick = @"click";
                 dispatch_async(dispatch_get_main_queue(), ^{
                     HyBidSKAdNetworkViewController *skAdnetworkViewController = [[HyBidSKAdNetworkViewController alloc] initWithProductParameters:productParams];
 
-                    UIViewController *navigationController = [UIApplication sharedApplication].keyWindow.rootViewController;
-                    UIViewController *topViewController = ((UINavigationController*)navigationController).visibleViewController;
-
-                    [topViewController presentViewController:skAdnetworkViewController animated:true completion:nil];
+                    [[UIApplication sharedApplication].topViewController presentViewController:skAdnetworkViewController animated:true completion:nil];
                 });
             } else {
                 [[UIApplication sharedApplication] openURL:[NSURL URLWithString:self.clickUrl]];
