3e5b1bb8577a991884b594bbfa7440595e5b096d
Orkhan Alizada <orkhan.alizada@pubnative.net>
2020-10-16 17:57:20 +0200

    Rewarded video ViewController separated.

---
+++ PubnativeLite/PubnativeLiteDemo/Standalone/PNLiteDemoPNLiteRewardedViewController.h
@@ -0,0 +1,14 @@
+//
+//  PNLiteDemoPNLiteRewardedViewController.h
+//  HyBid
+//
+//  Created by Orkhan Alizada on 16.10.20.
+//  Copyright © 2020 Can Soykarafakili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PNLiteDemoBaseViewController.h"
+
+@interface PNLiteDemoPNLiteRewardedViewController : PNLiteDemoBaseViewController
+
+@end
