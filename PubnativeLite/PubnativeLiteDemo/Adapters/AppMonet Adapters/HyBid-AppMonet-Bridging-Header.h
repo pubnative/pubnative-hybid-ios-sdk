e74ca250bca46c2da86ec0518b0a38c5f99d350d
Can Soykarafakılı <can.soykarafakili@gmail.com>
2020-11-06 18:40:31 +0100

    HyBid-AppMonet-Bridging-Header.h is created

---
+++ PubnativeLite/PubnativeLiteDemo/Adapters/AppMonet Adapters/HyBid-AppMonet-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "AppMonet+MoPub.h"
+#import "AppMonet+DFP.h"
+#import "AMCustomEventBanner.h"
+#import "AMCustomEventInterstitial.h"
+#import "AMCustomEventNative.h"
+#import "AMNativeAdRenderer.h"
