f0f027866d1868c1553fe17b1d9ea5537fbc8db6
Can Soykarafakılı <can.soykarafakili@gmail.com>
2020-12-16 18:04:21 +0100

    Prep for 2.4.1-beta

--- PubnativeLite/PubnativeLiteDemo/Adapters/MoPubAdapter/HyBidAdapterConfiguration.m
+++ PubnativeLite/PubnativeLiteDemo/Adapters/MoPubAdapter/HyBidAdapterConfiguration.m
@@ -23,8 +23,8 @@
 #import "HyBidAdapterConfiguration.h"
 
 NSString *const HyBidAdapterConfigurationNetworkName = @"pubnative";
-NSString *const HyBidAdapterConfigurationAdapterVersion = @"2.4.0.0-beta";
-NSString *const HyBidAdapterConfigurationNetworkSDKVersion = @"2.4.0-beta";
+NSString *const HyBidAdapterConfigurationAdapterVersion = @"2.4.1.0-beta";
+NSString *const HyBidAdapterConfigurationNetworkSDKVersion = @"2.4.1-beta";
 NSString *const HyBidAdapterConfigurationAppTokenKey = @"pubnative_appToken";
 
 @implementation HyBidAdapterConfiguration
