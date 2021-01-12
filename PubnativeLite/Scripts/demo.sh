3512850b2e4d58831dee1fb636c2600d4801eae3
Can Soykarafakılı <can.soykarafakili@gmail.com>
2021-01-13 00:55:59 +0100

    Trying to generate an .app file

---
+++ PubnativeLite/Scripts/demo.sh
@@ -0,0 +1,15 @@
+# Variable Declarations
+BASE_DIR=/tmp/circleci-artifacts
+PRODUCT_NAME=HyBidDemo
+HYBID_DEMO_APP_NAME=$PRODUCT_NAME.app
+HYBID_DEMO_APP=$BASE_DIR/$HYBID_DEMO_APP_NAME
+HYBID_DEMO_APP_ZIP_PATH=$BASE_DIR/HyBidDemo.app.zip
+
+# Show Current Versions
+xcodebuild -showsdks
+
+# Generate HyBid Demo App
+xcodebuild -arch x86_64 -sdk iphonesimulator13 -workspace HyBid.xcworkspace -scheme HyBidDemo -output $HYBID_DEMO_APP
+
+# Create a .zip HyBid Demo App
+zip -r $HYBID_DEMO_APP_ZIP_PATH $HYBID_DEMO_APP
