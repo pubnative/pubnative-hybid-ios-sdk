cda6c8fbc6c117ff5c338b4f46d963c49ab61860
Can Soykarafakılı <can.soykarafakili@gmail.com>
2021-01-13 02:15:33 +0100

    altering zip location

--- PubnativeLite/Scripts/demo.sh
+++ PubnativeLite/Scripts/demo.sh
@@ -2,14 +2,15 @@
 BASE_DIR=/tmp/circleci-artifacts
 PRODUCT_NAME=HyBidDemo
 HYBID_DEMO_APP_NAME=$PRODUCT_NAME.app
-HYBID_DEMO_APP=$BASE_DIR/$HYBID_DEMO_APP_NAME
+HYBID_DEMO_APP_PATH=$BASE_DIR/hyBidDemo
+HYBID_DEMO_APP=$HYBID_DEMO_APP_PATH/$HYBID_DEMO_APP_NAME
 HYBID_DEMO_APP_ZIP_PATH=$BASE_DIR/HyBidDemo.app.zip
 
 # Show Current Versions
 xcodebuild -showsdks
 
 # Generate HyBid Demo App
-xcodebuild -arch x86_64 -sdk iphonesimulator -workspace HyBid.xcworkspace -scheme HyBidDemo -verbose
+xcodebuild -arch x86_64 -sdk iphonesimulator -workspace HyBid.xcworkspace -scheme HyBidDemo CONFIGURATION_BUILD_DIR=$HYBID_DEMO_APP_PATH -verbose
 
 # Create a .zip HyBid Demo App
-zip -r $HYBID_DEMO_APP_ZIP_PATH /tmp/circleci-artifacts/HyBidDemo.app
+zip -r $HYBID_DEMO_APP_ZIP_PATH $HYBID_DEMO_APP
