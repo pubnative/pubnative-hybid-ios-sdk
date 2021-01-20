3104658e8c5d88e86962a6b2daee7fa6eda17f21
Can Soykarafakılı <can.soykarafakili@gmail.com>
2021-01-20 01:53:37 +0100

    development branch is matched with master in terms of archs and version

--- PubnativeLite/PubnativeLiteDemo/Adapters/HyBid-Bridging-Header.h
+++ PubnativeLite/PubnativeLiteDemo/Adapters/HyBid-Bridging-Header.h
@@ -14,6 +14,7 @@
 #import "HyBidMoPubMediationBannerCustomEvent.h"
 #import "HyBidMoPubMediationMRectCustomEvent.h"
 #import "HyBidMoPubMediationInterstitialCustomEvent.h"
+#import "HyBidMoPubMediationRewardedAdCustomEvent.h"
 #import "HyBidDFPUtils.h"
 #import "HyBidDFPHeaderBiddingLeaderboardCustomEvent.h"
 #import "HyBidDFPHeaderBiddingBannerCustomEvent.h"
