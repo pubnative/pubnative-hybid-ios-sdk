1451f73a0c8c8922f3941e68b272225f463dc064
Fares Benhamouda <fares.bnhamouda@gmail.com>
2021-03-23 15:02:38 +0100

    version bump HyBid edge beta3.

--- PubnativeLite/PubnativeLiteDemo/Adapters/AdMobAdapter/HyBidAdMobMediationRewardedCustomEvent.m
+++ PubnativeLite/PubnativeLiteDemo/Adapters/AdMobAdapter/HyBidAdMobMediationRewardedCustomEvent.m
@@ -104,7 +104,7 @@ typedef id<GADMediationRewardedAdEventDelegate> _Nullable(^HyBidAdMobMediationRe
     [self.delegate reportImpression];
 }
 
-// v: 2.4.3-edge-beta2
+// v: 2.4.3-edge-beta3
 + (GADVersionNumber)adSDKVersion {
     GADVersionNumber version = {0};
     version.majorVersion = 2;
