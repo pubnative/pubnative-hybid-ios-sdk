4b93d59223b48ce1f41fbcee9849c5013c43b397
Orkhan Alizada <orkhan.alizada@pubnative.net>
2023-01-02 13:29:45 +0400

    Release 2.16.2-beta12

--- PubnativeLite/PubnativeLiteDemo/Adapters/GAD (Previously AdMob) Adapter/HyBidGADUtils.m
+++ PubnativeLite/PubnativeLiteDemo/Adapters/GAD (Previously AdMob) Adapter/HyBidGADUtils.m
@@ -58,7 +58,7 @@ NSString *const HyBidGADAdapterKeyAppToken = @"pn_app_token";
     return result;
 }
 
-// v: 2.16.2-beta7
+// v: 2.16.2-beta12
 + (GADVersionNumber)adSDKVersion {
     GADVersionNumber version = {0};
     version.majorVersion = 2;
